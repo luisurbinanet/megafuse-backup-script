67eb835e957cf8d6034342a6cdbe80b65cb85f0d
Luis Urbina <luis@luisurbina.net>
2019-03-30 15:52:53 -0400

    Add files via upload
    Actualización Funcional para separar aún más las carpetas de respaldo de cada sitio web en vez de cada cliente nuestro y la implementación de variables para el ciclo de espera.
    Ejemplo del archivo de configuración de MegaFuse.

--- megafusebkp.sh
+++ megafusebkp.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Diego Cordoba - @d1cor / juncotic.com
-# Modificado y adaptado Luis Urbina
+# Modificado y adaptado Luis Urbina             en: 2019-03-30
 
 # Nombre del servidor sobre el que estamos realizando el backup
-SERVER="servidor"
+SERVER="NOMBRE-DE-TU-SERVIDOR"
 DEBUG="true"
 
 # Directorio temporal de backup (para tar)
 TMP_DIR="/tmp/backupsmega"
-DAYS_TO_BACKUP=7
+DAYS_TO_BACKUP=15
+
+# Tiempo de Espera a que finalice la copia entre tu SERVIDOR y MEGA
+# Si tu servidore esta en Internet, con 600 segundos (10 minutos) es mas que suficiente
+STANDBY_SECONDS=600
 BACKUP_MYSQL="true"
  
 # Credenciales de acceso a la DB
@@ -49,7 +53,7 @@ then
         do
                 if [ "${DEBUG}" = "true" ]
                 then
-                    echo "Procesando ${db}"
+                    echo "  \___: ${db}"
                 fi
                 mysqldump --opt -u${MYSQL_USER} -p${MYSQL_PASSWORD} "${db}" | gzip > ${TMP_DIR}/mysql/${db}_$(date +%F_%T).sql.gz
         done
@@ -59,21 +63,30 @@ fi
 # Backup websites
 if [ "${DEBUG}" = "true" ]
 then
-    echo "\ Websites"
+    echo ""
+    echo "\_ Backup de Websites"
 fi
 mkdir ${TMP_DIR}/websites
 for dir in $(find ${WEBS_DIR} -mindepth 1 -maxdepth 1 -type d)
 do
+    ## NOTA IMPORTANTE
+    ##
     ## Me interesan que los Websites de los clientes esten por separado y no juntos
     ## y como la carpeta donde estan los websites es /var/www/clients pregunto por ella
-    ## despues tomo cada subcarpeta (cliente) y lo compacto
+    ## despues tomo cada subcarpeta (cliente) y en mi caso la estructura es la siguiente
+    ## /var/www/clients/client(n)/web(m)
+    ## donde n -> es el Nro de Cliente y m -> es el Nro de la web
+    ## ME INTERESA COPIAR CADA WEB POR SEPARADO y no todo junto por cliente
+    ## si un cliente tiene 2 sitios webs, pues habran dos carpetas web(n) 
+    ## por ejemplo: web1 y web 18
+    ##
     if [ "${dir}" = "/var/www/clients" ]
     then
-        for clients in $(find ${dir} -mindepth 1 -maxdepth 1 -type d)
+        for clients in $(find ${dir} -mindepth 2 -maxdepth 2 -type d)
         do
             if [ "${DEBUG}" = "true" ]
                 then
-                echo "  \-- Procesando dir: ${clients}"
+                echo "  \___: ${clients}"
             fi
             cd $(dirname ${clients})
             sudo tar czf ${TMP_DIR}/websites/$(basename ${clients}).tar.gz $(basename ${clients})
@@ -82,7 +95,7 @@ do
     else
         if [ "${DEBUG}" = "true" ]
             then
-            echo "  \-- Procesando dir: ${dir}"
+            echo "  \___: ${dir}"
         fi
         cd $(dirname ${dir})
         sudo tar czf ${TMP_DIR}/websites/$(basename ${dir}).tar.gz $(basename ${dir})
@@ -104,13 +117,13 @@ fi
 sudo umount megafuse
 $MEGA_BIN -c $MEGA_CONF &> /dev/null &
 
-## Esperamos  a que se monte para seguir
+## Esperamos  a que se monte para seguir en la mayoria de los casos con 5 segundos basta (sleep 5)
 while  [ $(mount|grep megafuse|wc -l) -ne 1 ]; do
     if [ "${DEBUG}" = "true" ]
     then
         echo "esperando que mega monte..."
     fi
-    sleep 1
+    sleep 5
 done
 
 ## si no está creado el dir del backup del server en Mega, lo creamos
@@ -118,10 +131,10 @@ done
 
 if [ "${DEBUG}" = "true" ]
 then
-    echo "Eliminando Archivos viejos del dir montado..."
+    echo "Eliminando Archivos > ${DAYS_TO_BACKUP} dias de antiguedad de Mega..."
 fi
 
-## Busco los archivos que tengan mas de "n" dyas de antiguedad y los borro de Mega
+## Busco los archivos que tengan mas de "n" dias de antiguedad y los borro de Mega
 find $MOUNTPOINT/backup_${SERVER}/ -mindepth 1 -maxdepth 1 -type d -daystart -mtime ${DAYS_TO_BACKUP} -exec rm -rf {} +
 sync
  
@@ -133,7 +146,11 @@ cp -r --no-preserve=mode,ownership ${TMP_DIR} $MOUNTPOINT/backup_${SERVER}/$DATE
 sync; sync; sync
 
 # Esperamos un tiempo prudencial para desmontar el directorio y limpiar los temporales
-sleep 600
+if [ "${DEBUG}" = "true" ]
+then
+    echo "Ciclo de espera de $STANDBY_SECONDS segundos"
+fi
+sleep $STANDBY_SECONDS
 
 if [ "${DEBUG}" = "true" ]
 then
@@ -142,5 +159,10 @@ fi
 sudo umount $MOUNTPOINT
 sudo rm -rf ${TMP_DIR}
 
+# Kill DBUS session daemon (workaround)
+if [ "${DEBUG}" = "true" ]
+then
+    echo "Eliminando proceso"
+fi
 # Matando al proceso de MegaFuse por si quedó algo dando vueltas...
 sudo kill -9 $(ps fax|grep .megafuse.conf|grep -v grep |awk -F' ' '{print $1}')
